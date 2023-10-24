3328228944b73122db846623f84e053f0a082c74
Aryosetowmn <81026942+Aryosetowmn@users.noreply.github.com>
2023-10-24 18:52:45 +0700

    Initial commit

---
+++ src/pages/PageMain.vue
@@ -0,0 +1,140 @@
+<template>
+  <q-layout style="color: #050f27">
+    <q-page-container>
+      <q-page>
+        <!-- part1 -->
+        <div class="q-pa-xl">
+          <div
+            class="row"
+            style="background-color: #f7f8f8; border-radius: 48px"
+          >
+            <div class="col-4 q-pa-lg">
+              <div>
+                <div
+                  class="satoshiMedium"
+                  style="font-size: 20px; line-height: 30px"
+                >
+                  Capital raised
+                </div>
+                <div
+                  class="satoshiBold"
+                  style="font-size: 71.673px; line-height: 84.704px"
+                >
+                  $3.5M+
+                </div>
+                <div
+                  class="satoshiRegular"
+                  style="font-size: 22px; line-height: 36px"
+                >
+                  Crafted for your comfort, to enhance the beauty of your
+                  present lifestyle but also paves the way for a more
+                  aesthetically pleasing and harmonious future for both you and
+                  your cherished family.
+                </div>
+              </div>
+              <div class="q-pt-xl">
+                <div
+                  class="satoshiMedium"
+                  style="font-size: 20px; line-height: 24px"
+                >
+                  Introduction
+                </div>
+                <div
+                  class="satoshiRegular q-py-md"
+                  style="font-size: 42px; line-height: 112%"
+                >
+                  A vision for <br />
+                  liveable,<br />
+                  sustainable &<br />
+                  affordable.
+                </div>
+                <div
+                  class="satoshiRegular q-py-md"
+                  style="font-size: 22px; line-height: 36px"
+                >
+                  <q-btn
+                    class="satoshiRegular"
+                    style="border-radius: 12px"
+                    color="black"
+                    label="Start Exploring"
+                    size="18px"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="col-8 q-pa-lg">
+              <div
+                class="satoshiBold"
+                style="font-size: 90px; line-height: 107%"
+              >
+                We craft the future dwelling.
+              </div>
+              <div class="q-pt-lg">
+                <q-img
+                  style="border-radius: 48px; height: 600px"
+                  src="../assets/part1img.png"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- part2 -->
+        <div class="row q-pa-xl">
+          <div class="col-3 q-pr-md">
+            <q-img style="border-radius: 48px" src="../assets/part2img.jpeg" />
+          </div>
+          <div class="col-9 q-pl-md">
+            <div
+              class="full-height"
+              style="background-color: #f7f8f8; border-radius: 48px"
+            >
+              <div class="q-pa-lg text-right">
+                <div>
+                  <div
+                    class="satoshiMedium"
+                    style="font-size: 20px; line-height: 24px"
+                  >
+                    Satisfied Clients
+                  </div>
+                </div>
+                <div>
+                  <div
+                    class="satoshiBold"
+                    style="font-size: 42px; line-height: 84.704px"
+                  >
+                    8.4M+
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div
+                  class="satoshiBold"
+                  style="
+                    color: #050f27;
+                    font-size: 44px;
+                    padding-left: 128px;
+                    padding-right: 128px;
+                  "
+                >
+                  We can bring to life everything you've ever envisioned and
+                  dreamed of – your ideals and desires are our creations.
+                </div>
+                <div
+                  class="satoshiRegular"
+                  style="padding-left: 128px; padding-right: 128px font-size:22px"
+                >
+                  Our purpose is to transform your wildest dreams into reality.
+                  Regardless of how unconventional your vision may be, we
+                  possess the capability to craft your dream home, fulfilling
+                  your every desire.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script></script>
